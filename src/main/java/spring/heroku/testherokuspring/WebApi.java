247f0bf532ea62ba08c5cc0e6acd1e17bcde9628
XeroJCG <48506192+XeroJCG@users.noreply.github.com>
2021-03-13 21:38:21 -0600

    Update WebApi.java

--- src/main/java/spring/heroku/testherokuspring/WebApi.java
+++ src/main/java/spring/heroku/testherokuspring/WebApi.java
@@ -21,9 +21,6 @@ public class WebApi {
     @ResponseBody
     public String getRandomValue(){
         int rnd = (int)(Math.random() * (100 - 50 + 1) + 50);
-        int  hola = rnd;
-        int jr = 1000;
-        String harold;
         return Integer.toString(rnd);
     }
     
